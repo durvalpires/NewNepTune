bcb6c7969e7d27654fff0b2d3bca0ff95a8c66d7
Deniz Taylan İpek <armutsurat123@gmail.com>
2024-03-06 18:27:52 +0300

    Notes added, Level1 scene updated, popup image updated
    emirhanigotten

--- Assets/Scripts/PopUpToggleManager.cs
+++ Assets/Scripts/PopUpToggleManager.cs
@@ -1,3 +1,5 @@
+using Audio;
+using Enums;
 using UnityEngine;
 
 public class PopUpToggleManager : MonoBehaviour
@@ -5,18 +7,23 @@ public class PopUpToggleManager : MonoBehaviour
     public GameObject popup1; // Kontrol etmek istediğiniz GameObject
     public GameObject popup2;
     public GameObject popup3;
-
+    public GameObject popUpCanvas;
+    
     public void TogglePopup1()
     {
         // GameObject'in şu anki aktiflik durumunun tersini ayarlayın
+        popUpCanvas.SetActive(true);
         popup1.SetActive(!popup1.activeSelf);
+        AudioManager.Instance.PlaySFX(SoundList.note_C);
     }
     public void TogglePopup2()
     {
+        popUpCanvas.SetActive(true);
         popup2.SetActive(!popup2.activeSelf);
     }
     public void TogglePopup3()
     {
+        popUpCanvas.SetActive(true);
         popup3.SetActive(!popup3.activeSelf);
     }
 }
