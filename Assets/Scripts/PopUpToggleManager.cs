80d6711d0a95b036b38f00aefdc5e4a86bd19830
Deniz Taylan İpek <armutsurat123@gmail.com>
2024-03-06 22:50:58 +0300

    Level1 popups completed, bugfix

--- Assets/Scripts/PopUpToggleManager.cs
+++ Assets/Scripts/PopUpToggleManager.cs
@@ -1,6 +1,8 @@
+using System;
 using Audio;
 using Enums;
 using UnityEngine;
+using UnityEngine.Analytics;
 
 public class PopUpToggleManager : MonoBehaviour
 {
@@ -8,22 +10,44 @@ public class PopUpToggleManager : MonoBehaviour
     public GameObject popup2;
     public GameObject popup3;
     public GameObject popUpCanvas;
+    private bool _popUpActive;
     
+    private void Update()
+    {
+        if (_popUpActive && Input.GetMouseButtonDown(0))
+        {
+          Back();
+        }
+               
+    }
+
     public void TogglePopup1()
     {
         // GameObject'in şu anki aktiflik durumunun tersini ayarlayın
         popUpCanvas.SetActive(true);
-        popup1.SetActive(!popup1.activeSelf);
-        AudioManager.Instance.PlaySFX(SoundList.note_C);
+        popup1.SetActive(true);
+        StartCoroutine(AudioManager.Instance.PlaySFX(SoundList.note_C, 1f));
+        _popUpActive = true;
     }
     public void TogglePopup2()
     {
         popUpCanvas.SetActive(true);
-        popup2.SetActive(!popup2.activeSelf);
+        popup2.SetActive(true);
+        StartCoroutine(AudioManager.Instance.PlaySFX(SoundList.note_C, 1f));
+        _popUpActive = true;
     }
     public void TogglePopup3()
     {
         popUpCanvas.SetActive(true);
-        popup3.SetActive(!popup3.activeSelf);
+        popup3.SetActive(true);
+        _popUpActive = true;
+    }
+
+    private void Back()
+    {
+        popup1.SetActive(false);
+        popup2.SetActive(false);
+        popUpCanvas.SetActive(!popUpCanvas.activeSelf);
+        _popUpActive = false;
     }
 }
