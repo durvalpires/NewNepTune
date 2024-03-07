5086f77546b9dcdb8dcacadf8008029c83d9df03
Mengart <sesabuncu@hotmail.com>
2024-03-07 14:45:53 +0300

    yatay pianoyu yatırdık

--- Assets/Scripts/Instruments/PianoButtons.cs
+++ Assets/Scripts/Instruments/PianoButtons.cs
@@ -1,5 +1,8 @@
+using System;
 using Audio;
 using Enums;
+using Levels;
+using Levels.Level1Game1;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -7,19 +10,31 @@ namespace Instruments
 {
     public class PianoButtons : EventTrigger
     {
+        TriggerManagerG1L1 _triggerManagerG1L1;
+        ScoreBarSlider _scoreBarSlider;
+        
+        private void Start()
+        {
+            _triggerManagerG1L1 = FindObjectOfType<TriggerManagerG1L1>();
+            _scoreBarSlider = FindObjectOfType<ScoreBarSlider>();
+        }
+
         public override void OnPointerClick(PointerEventData data) //Pressed once
         {
-            Debug.Log("OnPointerClick called.");
         }
 
         public override void OnPointerDown(PointerEventData data) //Started pressing down
         {
+            if (_triggerManagerG1L1.DoNotaControl())
+            {
+                _scoreBarSlider.UpdateSliderBasedOnDivision();
+            }
+            
             AudioManager.Instance.sfxSource.Stop();
             AudioManager.Instance.sfxSource.volume = 1;
             
             if (_soundFadeOut != null) StopCoroutine(_soundFadeOut);
             
-            Debug.Log("OnPointerDown called.");
             AudioManager.Instance.sfxSource.clip = AudioManager.Instance.sfxSounds[2].clip; 
             //TODO : This above is hardcoded, it should be changed to a more dynamic way.
             AudioManager.Instance.sfxSource.Play();
@@ -29,7 +44,6 @@ namespace Instruments
         
         public override void OnPointerUp(PointerEventData data) //Stopped pressing down
         {
-            Debug.Log("OnPointerUp called.");
             _soundFadeOut = StartCoroutine(AudioManager.Instance.SoundFadeOut(AudioManager.Instance.sfxSource, .1f));
         }
     }
