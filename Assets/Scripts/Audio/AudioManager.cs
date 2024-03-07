b0551f5ec2d2b9b3e1d7083a9d0df5cd798de31a
Mengart <sesabuncu@hotmail.com>
2024-03-07 18:43:48 +0300

    Revert "no message"
    This reverts commit 9ceecf1b0876e868cffbdbdb8b8b25eb9ceb1bcc.

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -62,11 +62,6 @@ namespace Audio
             }
         }
         
-        public bool CheckIfMusicIsPlaying()
-        {
-            return musicSource.isPlaying;
-        }
-
         public IEnumerator WaitForSeconds(float waitTime)
         {
             yield return new WaitForSeconds(waitTime);
