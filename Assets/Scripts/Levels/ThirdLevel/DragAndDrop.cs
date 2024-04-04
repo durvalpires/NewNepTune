5c008a963c49e7daefd4d25b7188c799e119fc30
Selim Sabuncu <sesabuncu@hotmail.com>
2024-04-04 04:55:25 +0300

    İlk defa 102 pending files olarak push atıyorum ii iş. MusicGuess,InstrumentGuess,SelectionMinigames updated to be modular and put on levels.

--- Assets/Scripts/Levels/ThirdLevel/DragAndDrop.cs
+++ Assets/Scripts/Levels/ThirdLevel/DragAndDrop.cs
@@ -1,58 +1,61 @@
 using UnityEngine;
 
-public class DragAndDrop : MonoBehaviour
+namespace Levels.ThirdLevel
 {
-    private bool isDragging = false;
-    private Vector3 offset;
-    public Vector3 snapPosition; // Hedef konum, Inspector'dan ayarlanabilir
-    public float snapRadius = 0.5f; // Yapışma mesafesi
-    public static int magneticTracker = 6;
-    public GameObject congratsText;
-    [Header("Sounds")]
-    public AudioSource DraggingSound;
-    public AudioClip DragDoneSound;
-
-    void Update()
+    public class DragAndDrop : MonoBehaviour
     {
-        if (isDragging)
+        private bool isDragging = false;
+        private Vector3 offset;
+        public Vector3 snapPosition; // Hedef konum, Inspector'dan ayarlanabilir
+        public float snapRadius = 0.5f; // Yapışma mesafesi
+        public static int magneticTracker = 6;
+        public GameObject congratsText;
+        [Header("Sounds")]
+        public AudioSource DraggingSound;
+        public AudioClip DragDoneSound;
+
+        void Update()
         {
-            Vector3 currentPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
-            currentPosition.z = 0;
-            transform.position = currentPosition;
-            //Debug.Log(currentPosition);
-        }
-    }
-
-    void OnMouseDown()
-    {
-        if (!isDragging)
-        {
-            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            offset = transform.position - mousePosition;
-            offset.z = 0;
-            isDragging = true;
-        }
-    }
-
-    void OnMouseUp()
-    {
-        isDragging = false;
-        SnapToPosition();
-    }
-
-    void SnapToPosition()
-    {
-        float distance = Vector3.Distance(transform.position, snapPosition);
-        if (distance <= snapRadius)
-        {
-            transform.position = snapPosition;
-            magneticTracker--;
-            Debug.Log(magneticTracker);
-            DraggingSound.PlayOneShot(DragDoneSound);
-            if(magneticTracker <= 0)
+            if (isDragging)
             {
-                Debug.Log("oyun bitti");
-                congratsText.SetActive(true);
+                Vector3 currentPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
+                currentPosition.z = 0;
+                transform.position = currentPosition;
+                //Debug.Log(currentPosition);
+            }
+        }
+
+        void OnMouseDown()
+        {
+            if (!isDragging)
+            {
+                Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                offset = transform.position - mousePosition;
+                offset.z = 0;
+                isDragging = true;
+            }
+        }
+
+        void OnMouseUp()
+        {
+            isDragging = false;
+            SnapToPosition();
+        }
+
+        void SnapToPosition()
+        {
+            float distance = Vector3.Distance(transform.position, snapPosition);
+            if (distance <= snapRadius)
+            {
+                transform.position = snapPosition;
+                magneticTracker--;
+                Debug.Log(magneticTracker);
+                DraggingSound.PlayOneShot(DragDoneSound);
+                if(magneticTracker <= 0)
+                {
+                    Debug.Log("oyun bitti");
+                    congratsText.SetActive(true);
+                }
             }
         }
     }
---
+++ Assets/Scripts/Minigames/InstrumentGuess.cs
@@ -0,0 +1,142 @@
+using System.Collections;
+using Audio;
+using Enums;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Minigames
+{
+   public class InstrumentGuess : MonoBehaviour
+   {
+      [Header("References for Scene Set Up")]
+      [SerializeField] private Button backButton;
+      [SerializeField] private Button finishedBackButton;
+      [SerializeField] private string levelToReturn;
+
+      [Header("References for Minigame Set Up")]
+      public GameObject winPanel;
+      public GameObject losePanel;
+      public GameObject finishPanel;
+
+      [SerializeField] private GameObject[] gameLevels;
+      [SerializeField] private AudioClip[] audioClips;
+      [SerializeField] private Sprite[] correctAnswerSprites;
+      private AudioSource _audioSource;
+      
+      private Sprite[] _sprites;
+      
+      private int _currentLevel;
+      
+      private void Start()
+      {
+         backButton.GetComponent<Button>().onClick.AddListener(() =>
+         {
+            SceneManager.LoadScene(levelToReturn);
+         });
+         finishedBackButton.GetComponent<Button>().onClick.AddListener(() =>
+         {
+            SceneManager.LoadScene(levelToReturn);
+         });
+         
+         _sprites = Resources.LoadAll<Sprite>("InstrumentPNGs");
+         
+         for (int i = 0; i < gameLevels.Length; i++)
+         {
+            SetUpLevel(i);
+            gameLevels[i].gameObject.SetActive(false);
+         }
+         gameLevels[0].gameObject.SetActive(true);
+         
+         _audioSource = gameObject.GetComponent<AudioSource>();
+         _audioSource.clip = audioClips[_currentLevel];
+         
+         StartCoroutine(PlayAfterSeconds(.8f));
+      }
+
+      private IEnumerator PlayAfterSeconds(float seconds)
+      {
+         yield return new WaitForSeconds(seconds);
+         _audioSource.Play();
+      }
+      
+      public void TrueAnswer()
+      {
+         winPanel.SetActive(true);
+         AudioManager.Instance.PlaySFX(SoundList.WinSound);
+         gameLevels[_currentLevel].gameObject.SetActive(false);
+      }
+
+      public void FalseAnswer()
+      {
+         losePanel.SetActive(true);
+         gameLevels[_currentLevel].gameObject.SetActive(false);
+         AudioManager.Instance.PlaySFX(SoundList.LoseSound);
+      }
+
+      private void SetUpLevel(int levelToSet)
+      {
+         GameObject topButton = gameLevels[levelToSet].transform.GetChild(0).gameObject;
+         GameObject bottomButton = gameLevels[levelToSet].transform.GetChild(1).gameObject;
+      
+         bool isLeftCorrect = System.Guid.NewGuid().GetHashCode() % 2 == 0;
+      
+         //int randomIndex = Random.Range(0, _sprites.Length);
+         //Sprite randomSprite = _sprites[randomIndex];
+      
+         Sprite randomSprite = correctAnswerSprites[levelToSet];
+         while (randomSprite == correctAnswerSprites[levelToSet])
+         {
+            int randomIndex = Random.Range(0, _sprites.Length);
+            randomSprite = _sprites[randomIndex];
+         }
+         
+         // Set the sprite of the correct button
+         if (isLeftCorrect)
+         {
+            //Debug.Log($"Left is correct. {levelToSet + 1}");
+            
+            topButton.GetComponent<Image>().sprite = correctAnswerSprites[levelToSet];
+            topButton.GetComponent<Button>().onClick.AddListener(TrueAnswer);
+      
+            bottomButton.GetComponent<Image>().sprite = randomSprite;
+            bottomButton.GetComponent<Button>().onClick.AddListener(FalseAnswer);    
+         }
+         else
+         {
+            //Debug.Log($"Right is correct. {levelToSet + 1}");
+            
+            bottomButton.GetComponent<Image>().sprite = correctAnswerSprites[levelToSet];
+            bottomButton.GetComponent<Button>().onClick.AddListener(TrueAnswer);
+      
+            topButton.GetComponent<Image>().sprite = randomSprite;
+            topButton.GetComponent<Button>().onClick.AddListener(FalseAnswer);
+         }
+      }
+      
+      public void NextLevelButton()
+      {
+         if (_currentLevel + 1 < gameLevels.Length)
+         {
+            _currentLevel++;
+            gameLevels[_currentLevel].gameObject.SetActive(true);
+            _audioSource.clip = audioClips[_currentLevel];
+            winPanel.SetActive(false);
+            _audioSource.Play();
+         }
+         else if (_currentLevel <= gameLevels.Length)
+         {
+            winPanel.SetActive(false);
+            finishPanel.SetActive(true);
+         }
+      }
+      
+      public void Restart()
+      {
+         winPanel.SetActive(false);
+         losePanel.SetActive(false);
+         gameLevels[_currentLevel].gameObject.SetActive(true);
+         _audioSource.Play();
+      }
+   }
+}
