145de29d116efceb64508cc747fa19a7d6c4e3f6
Selim Sabuncu <sesabuncu@hotmail.com>
2024-09-02 17:09:28 +0300

    Added new scenes for the UI rework, necessary assets. New package "Auditor". Removed unnecassary files and/or sprites. Started an update on the memoryCardGame but sidetracked to more importent subjects.

---
+++ Assets/Scripts/Minigames/MemoryCardManager.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Minigames
+{
+    public class MemoryCardManager : MonoBehaviour
+    {
+        [SerializeField] private const int gridRows = 2;
+        [SerializeField] private const int gridCols = 4;
+        [SerializeField] private const float offsetX = 2f;
+        [SerializeField] private const float offsetY = 2.5f;
+
+        [SerializeField] private MemoryCard originalCard;
+        [SerializeField] private Sprite[] images;
+        [SerializeField] private TMP_Text scoreLabel;
+
+        private MemoryCard _firstRevealed;
+        private MemoryCard _secondRevealed;
+
+        private int _score = 0;
+    
+        public bool CanReveal
+        {
+            get { return _secondRevealed == null; }
+        }
+
+        public void CardRevealed(MemoryCard card)
+        {
+            if (_firstRevealed == null)
+            {
+                _firstRevealed = card;
+            }
+            else
+            {
+                _secondRevealed = card;
+                StartCoroutine(CheckMatch());
+            }
+        }
+    
+        private IEnumerator CheckMatch()
+        {
+            if (_firstRevealed.Id == _secondRevealed.Id)
+            {
+                _score++;
+                scoreLabel.text = $"Score: {_score}";
+            }
+            else
+            {
+                yield return new WaitForSeconds(.5f);
+                _firstRevealed.Unreveal();
+                _secondRevealed.Unreveal();
+            }
+            _firstRevealed = null;
+            _secondRevealed = null;
+        }
+    
+        private void Start()
+        {
+            Vector3 startPos = originalCard.transform.position;
+
+            int[] numbers = { 0, 0, 1, 1, 2, 2, 3, 3 };
+            numbers = ShuffleArray(numbers);
+
+            for (int i = 0; i < gridCols; i++)
+            {
+                for (int j = 0; j < gridRows; j++)
+                {
+                    MemoryCard card;
+                    if (i == 0 && j == 0)
+                    {
+                        card = originalCard;
+                    }
+                    else
+                    {
+                        card = Instantiate(originalCard) as MemoryCard;
+                    }
+
+                    int index = j * gridCols + i;
+                    int id = numbers[index];
+                    card.SetCard(id, images[id]);
+                    float posX = (offsetX * i) + startPos.x;
+                    float posY = -(offsetY * j) + startPos.y;
+                    card.transform.position = new Vector3(posX, posY, startPos.z);
+                }
+            }
+        }
+
+        private int[] ShuffleArray(int[] numbers)
+        {
+            int[] newArray = numbers.Clone() as int[];
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                int tmp = newArray[i];
+                int r = Random.Range(i, newArray.Length);
+                newArray[i] = newArray[r];
+                newArray[r] = tmp;
+            }
+            return newArray;
+        }
+    
+        public void Restart()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
