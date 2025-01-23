24779a5336761d6c44a36fee5562304e8c6b1038
durvalpires <durvalp1@gmail.com>
2025-01-23 13:28:25 +0100

    Settings work

---
+++ Assets/_App-v2/Scripts/MainMenu/SettingsController.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingsController : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject settingsPanel;
+
+
+    public void OpenSettings()
+    {
+        settingsPanel.SetActive(true);
+    }
+    
+    public void CloseSettings()
+    {
+        settingsPanel.SetActive(false);
+    }
+
+    public void ResetProgress()
+    {
+        PlayerModel.ClearData();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    
+}
